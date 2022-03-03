9d806a569684dda5db9fc13e3156b4c983dba355
hkonsti <45428767+hkonsti@users.noreply.github.com>
2022-03-03 18:28:39 +0100

    seperate camera logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,23 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
-import {QrScanner} from './QrScanner';
+import {QrScanner} from "./QrScanner";
+import {Camera} from "./Camera";
 
-interface AppState {
-	activeCamera: number,
-	errorMessage: string,
-}
-
-export default class App extends React.Component<{}, AppState> {
+export default class App extends React.Component<{}, {}> {
 
 	scanner: QrScanner;
+	camera: Camera;
 
 	constructor(props: any) {
 		super(props);
 
-		this.state = {
-			activeCamera: -1,
-			errorMessage: "",
-		};
-
 		this.scanner = new QrScanner();
+		this.camera = new Camera();
 	}
 
 	async componentDidMount() {
-		// permission
-		// github guy does it differently
-		// https://github.com/bensonruan/webcam-easy/blob/0ebfea62fec456c9b618d5f17c72386124e753b5/dist/webcam-easy.js#L49
-		const stream = await navigator.mediaDevices.getUserMedia({video: true, audio: false});
-
-		// get all devices
-		const devices = await navigator.mediaDevices.enumerateDevices();
-
-		// iterate and only grab video devices
-		const videoDevies = [];
-		devices.forEach(device => {
-			if (device.kind === "videoinput") {
-				videoDevies.push(device);
-			}
-		});
-
-		if (videoDevies.length < 1) {
-			this.setState({
-				errorMessage: "Device doesn't have a camera.",
-			})
-			return;
-		}
-
-		// get current webcam id
-		this.setState({activeCamera: 0});
-		const deviceId = devices[this.state.activeCamera].deviceId;
-
-		// get dom elements to project on
 		const videoElement = document.getElementById("webcam") as HTMLVideoElement;
-
-		videoElement.srcObject = stream;
-		videoElement.play();
+		await this.camera.setUpStream(videoElement);
 
 		this.setUpRunner();
 	}
@@ -76,11 +39,7 @@ export default class App extends React.Component<{}, AppState> {
 		const canvasElement = document.getElementById("canvas") as HTMLCanvasElement;
 		const videoElement = document.getElementById("webcam") as HTMLVideoElement;
 
-		canvasElement.height = videoElement.height;
-		canvasElement.width = videoElement.width;
-		const context = canvasElement.getContext("2d");
-		context!.clearRect(0, 0, canvasElement.width, canvasElement.height);
-		context!.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
+		this.camera.drawImageOnCanvas(canvasElement, videoElement);
 
 		await this.scanner.scan(canvasElement);
 	}
@@ -88,7 +47,6 @@ export default class App extends React.Component<{}, AppState> {
 	render() {
 		return (
 			<div className="App">
-				{this.state.errorMessage}
 				<video id="webcam" autoPlay={true} playsInline={true} width={640} height={480}></video>
 				<canvas id="canvas" className="d-none"></canvas>
 				<button onClick={this.takePhoto}>
--- src/Camera.tsx
+++ src/Camera.tsx
@@ -1,5 +1,73 @@
+
+enum CameraStatus {
+	STREAM_RUNNING,
+	NO_ACCESS,
+	NO_CAMERA,
+}
+
 export class Camera {
 
-    
+	stream: MediaStream | undefined;
+	devices: MediaDeviceInfo[] = [];
+	currentCamerId: string | undefined;
 
+	/**
+	 * Asks for permission for camera stream and sets up stream.
+	 * @returns If permission was given by the user
+	 */
+	public async setUpStream(videoElement: HTMLVideoElement): Promise<CameraStatus> {
+		const hasPermission = await this.getPermissions();
+		if (!hasPermission) {
+			return CameraStatus.NO_ACCESS;
+		}
+
+		const deviceHasCamera = await this.getVideoDevices();
+		if (!deviceHasCamera) {
+			return CameraStatus.NO_CAMERA;
+		}
+
+		this.currentCamerId = this.devices[0].deviceId;
+		videoElement.srcObject = this.stream!;
+		videoElement.play();
+
+		return CameraStatus.STREAM_RUNNING;
+	}
+
+	/**
+	 * Asks camera permission from user and returns true if permission has been given, false otherwise.
+	 */
+	private async getPermissions(): Promise<boolean> {
+		try {
+			// https://github.com/bensonruan/webcam-easy/blob/0ebfea62fec456c9b618d5f17c72386124e753b5/dist/webcam-easy.js#L49
+			this.stream = await navigator.mediaDevices.getUserMedia({video: true, audio: false});
+			return true;
+		} catch (e) {
+			return false;
+		}
+	}
+
+	/**
+	 * Returns true, if the device has any video devices, false otherwise.
+	 */
+	private async getVideoDevices(): Promise<boolean> {
+		const devices = await navigator.mediaDevices.enumerateDevices();
+
+		const videoDevies = [];
+		devices.forEach(device => {
+			if (device.kind === "videoinput") {
+				videoDevies.push(device);
+			}
+		});
+
+		this.devices = devices;
+		return this.devices.length > 0;
+	}
+
+	public async drawImageOnCanvas(canvasElement: HTMLCanvasElement, videoElement: HTMLVideoElement) {
+		canvasElement.height = videoElement.height;
+		canvasElement.width = videoElement.width;
+		const context = canvasElement.getContext("2d");
+		context!.clearRect(0, 0, canvasElement.width, canvasElement.height);
+		context!.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
+	}
 }
