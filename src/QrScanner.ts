0e85a7a652d69d6ee83f440aa0de5b504b20e113
hkonsti <45428767+hkonsti@users.noreply.github.com>
2022-03-03 18:03:00 +0100

    formatting

--- src/QrScanner.ts
+++ src/QrScanner.ts
@@ -1,80 +1,80 @@
-import { OpenCV } from "./opencv/OpenCV";
+import {OpenCV} from "./opencv/OpenCV";
 
 interface Coordinate {
-    x: number;
-    y: number;
+	x: number;
+	y: number;
 }
 
 interface Rectangle {
-    upperLeft: Coordinate;
-    lowerRight: Coordinate;
+	upperLeft: Coordinate;
+	lowerRight: Coordinate;
 }
 
 export class QrScanner {
 
-    isCVInitialized: boolean = false;
-    
-    isFileInitialized: boolean = false;
-    path: string = "CASCADE_FILE";
+	isCVInitialized: boolean = false;
 
-    classifier: any;
+	isFileInitialized: boolean = false;
+	path: string = "CASCADE_FILE";
 
-    private async waitForFile(path: string, url: string): Promise<void> {
-        return new Promise((res, rej) => {
-            let utils = new Utils("errorMessage");
-            utils.createFileFromUrl(path, url, () => {
-                res();
-            });
-        });
-    }
+	classifier: any;
 
-    public async scan(image: HTMLCanvasElement): Promise<void> {
-        await this.findFIPs(image)
-    }
+	private async waitForFile(path: string, url: string): Promise<void> {
+		return new Promise((res, rej) => {
+			let utils = new Utils("errorMessage");
+			utils.createFileFromUrl(path, url, () => {
+				res();
+			});
+		});
+	}
 
-    private async findFIPs(image: HTMLCanvasElement): Promise<any> {
-        if (!this.isCVInitialized) {
-            await OpenCV.loadOpenCV();
-            this.isCVInitialized = true;
-        }
+	public async scan(image: HTMLCanvasElement): Promise<void> {
+		await this.findFIPs(image)
+	}
 
-        if (!this.isFileInitialized) {
-            let file = "./cv/cascade_selftrained_8.xml";
-            await this.waitForFile(this.path, file);
-            this.isFileInitialized = true;
-        }
+	private async findFIPs(image: HTMLCanvasElement): Promise<any> {
+		if (!this.isCVInitialized) {
+			await OpenCV.loadOpenCV();
+			this.isCVInitialized = true;
+		}
 
-        if (!this.classifier) {
-            this.classifier = new cv.CascadeClassifier();
-            this.classifier.load(this.path);
-        }
+		if (!this.isFileInitialized) {
+			let file = "./cv/cascade_selftrained_8.xml";
+			await this.waitForFile(this.path, file);
+			this.isFileInitialized = true;
+		}
 
-        let img = cv.imread(image);
+		if (!this.classifier) {
+			this.classifier = new cv.CascadeClassifier();
+			this.classifier.load(this.path);
+		}
 
-        // Convert to grayscale
-        let gray = new cv.Mat();
-        cv.cvtColor(img, gray, cv.COLOR_RGBA2GRAY);
+		let img = cv.imread(image);
 
-        let fips = new cv.RectVector();
-        let msize = new cv.Size(0,0);
-        this.classifier.detectMultiScale(gray, fips, 1.1, 3, 0, msize, msize);
+		// Convert to grayscale
+		let gray = new cv.Mat();
+		cv.cvtColor(img, gray, cv.COLOR_RGBA2GRAY);
 
-        console.log(fips.size());
+		let fips = new cv.RectVector();
+		let msize = new cv.Size(0, 0);
+		this.classifier.detectMultiScale(gray, fips, 1.1, 3, 0, msize, msize);
 
-        for (let i = 0; i < fips.size(); ++i) {
-            let p = fips.get(i);
+		console.log(fips.size());
 
-            let p1 = new cv.Point(p.x, p.y);
-            let p2 = new cv.Point(p.x + p.width, p.y + p.height);
+		for (let i = 0; i < fips.size(); ++i) {
+			let p = fips.get(i);
 
-            cv.rectangle(img, p1, p2, [255, 0, 0, 255]);
-        }
+			let p1 = new cv.Point(p.x, p.y);
+			let p2 = new cv.Point(p.x + p.width, p.y + p.height);
 
-        cv.imshow(image, img)
+			cv.rectangle(img, p1, p2, [255, 0, 0, 255]);
+		}
 
-        img.delete();
-        gray.delete();
-        fips.delete();
-    }
+		cv.imshow(image, img)
+
+		img.delete();
+		gray.delete();
+		fips.delete();
+	}
 
 }
--- src/opencv/OpenCV.tsx
+++ src/opencv/OpenCV.tsx
@@ -5,22 +5,22 @@ const OPENCV_UTILS = "http://localhost:3000/cv/utils.js";
 
 export class OpenCV {
 
-    static async loadOpenCV(): Promise<void> {
-        return new Promise(async (res, rej) => {
-            await injectScript("opencv-injected", OPENCV);
-            await injectScript("opencv-utils-injected", OPENCV_UTILS);
-            cv["onRuntimeInitialized"] = () => {
-                res();
-            }
-            setTimeout(() => {
-                rej("OpenCV took too long to initialize.");
-            }, 5000);
-        });
-    }
+	static async loadOpenCV(): Promise<void> {
+		return new Promise(async (res, rej) => {
+			await injectScript("opencv-injected", OPENCV);
+			await injectScript("opencv-utils-injected", OPENCV_UTILS);
+			cv["onRuntimeInitialized"] = () => {
+				res();
+			}
+			setTimeout(() => {
+				rej("OpenCV took too long to initialize.");
+			}, 5000);
+		});
+	}
 
-    static unloadOpenCV() {
-        // TODO: not needed as of right now since the whole app is using OpenCV
-    }
+	static unloadOpenCV() {
+		// TODO: not needed as of right now since the whole app is using OpenCV
+	}
 
 }
 
